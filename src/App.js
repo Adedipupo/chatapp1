c02a0ba9ea8a48c828e60410bb8f7e3bd7e76f2d
Adedipupo <ajokanola@terragonltd.com>
2022-11-01 17:37:05 +0100

    login-register-pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.scss';
+import { Home } from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 
 function App() {
   return (
     <div>
-      {/* <Register /> */}
-      <Login />
+     <Home/>
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div className="home">
+        <div className="container">
+            
+        </div>
+    </div>
+  )
+}
